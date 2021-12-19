d486d8413e925a934db27e19f8dab60a0752a11a
afjolhossain <you@example.com>
2021-12-19 12:16:36 +0600

    first commit

---
+++ src/pages/Shared/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Services</Nav.Link>
+            <Nav.Link href="#pricing">Medicines</Nav.Link>
+            <Nav.Link href="#pricing">Owner </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
