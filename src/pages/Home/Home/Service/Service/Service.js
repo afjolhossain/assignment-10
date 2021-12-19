514682d028b0fd86bf608c58dd20b932ad6da262
afjolhossain <you@example.com>
2021-12-19 13:27:31 +0600

    servicers-added

--- src/pages/Home/Home/Service/Service/Service.js
+++ src/pages/Home/Home/Service/Service/Service.js
@@ -1,9 +1,37 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const Service = ({ service }) => {
   const { name, price, img, details } = service;
 
-  return <div>{name}</div>;
+  return (
+    <div>
+      <Card sx={{ maxWidth: 345 }} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={img}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {details}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
 };
 
 export default Service;
--- src/pages/Home/Home/Service/Services/Services.js
+++ src/pages/Home/Home/Service/Services/Services.js
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Service from "../Service/Service";
 
@@ -9,10 +10,18 @@ const Services = () => {
       .then((data) => setServices(data.service));
   }, []);
   return (
-    <div id="service">
-      {services.map((service) => (
-        <Service key={service.id} service={service}></Service>
-      ))}
+    <div style={{ marginTop: 50 }} id="service">
+      <h1 style={{ marginBottom: 50 }}>Our Services</h1>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3, lg: 2 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        justifyContent="center"
+      >
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </Grid>
     </div>
   );
 };
